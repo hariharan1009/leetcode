6378de0ddda72534c8a9ef161abd9f02a0f09537
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2024-12-23 18:00:39 +0530

    Time: 0 ms (100%), Space: 41.8 MB (32.78%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,5 +3,15 @@ class Solution {
         String[] a=s.split(" ");
         String c=a[a.length-1];
         return c.length();
+        // int count=0;
+        // int a=s.length();
+        // for(int i=a-1;i>=0;i--){
+        //     if(s.charAt(i)==' ' && count>0)
+        //     break;
+        //     if(s.charAt(i)!=' ')
+        //     count+=1;
+        // }
+        // return count;
+
     }
 }
