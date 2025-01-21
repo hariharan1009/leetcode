fa9de101c110d8703ae764e96740285c15ab60e1
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-01-21 20:04:11 +0530

    Time: 0 ms (100%), Space: 13.7 MB (45.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # nums.sort()
+        # c=0
+        # for i in range(len(nums)-2):
+        #     if nums[i==nums[i+1]]:
+        #         c+=c
+        #     if c>len(nums)/2:
+        #         return nums[i]
+        #     else:
+        #         c=1
+        # a=nums[len(nums)/2]
+        # return a
         nums.sort()
-        c=0
-        for i in range(len(nums)-2):
-            if nums[i==nums[i+1]]:
-                c+=c
-            if c>len(nums)/2:
-                return nums[i]
-            else:
-                c=1
-        a=nums[len(nums)/2]
-        return a
-
+        return nums[len(nums)//2]
         
