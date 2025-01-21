f6fd8aadfadebbfad93aef34db90b2da42885ac6
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-01-21 20:02:11 +0530

    Time: 24 ms (9.71%), Space: 14.6 MB (7.22%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        c=0
+        for i in range(len(nums)-2):
+            if nums[i==nums[i+1]]:
+                c+=c
+            if c>len(nums)/2:
+                return nums[i]
+            else:
+                c=1
+        a=nums[len(nums)/2]
+        return a
+
+        
