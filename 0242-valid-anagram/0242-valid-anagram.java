e3766ea777d36b7333c26a7e025013190832e943
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-04-22 16:20:43 +0530

    Time: 13 ms (32%), Space: 44.6 MB (44.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
        HashMap<Character,Integer> map=new HashMap<>();
-       for(char i:s.toCharArray()){
-        map.put(i,map.getOrDefault(i,0)+1);
-       }
-        for(char i:t.toCharArray()){
-        map.put(i,map.getOrDefault(i,0)-1);
-       }
+        for(char c :s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)-1);
+        }
        for(int i:map.values()){
         if(i!=0){
             return false;
