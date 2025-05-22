d4829b14f48dfbc7c1c59e595f4263d72e72ba0d
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-05-22 07:20:39 +0530

    Time: 2 ms (94.51%), Space: 46.9 MB (34.74%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp=new int[n+1];
-        int x=1;
-        for(int i=1;i<=n;i++){
-            if(x*2==i){
-                x=i;
-            }
-            dp[i]=dp[i-x]+1;
+        int[] a=new int[n+1];
+        for(int i=0;i<=n;i++){
+            a[i]=a[i>>1]+(i&1);
         }
-        return dp;
+        return a;
     }
 }
