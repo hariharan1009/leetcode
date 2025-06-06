7da6d90f7801cf663b63b333576ee5ca910cb0fe
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-06-06 20:10:03 +0530

    Time: 0 ms (100%), Space: 41 MB (69.29%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,15 +18,13 @@ class Solution {
         if(p==null && q==null){
             return true;
         }
-        else if(p==null || q==null){
+        if(p==null ||q==null){
             return false;
         }
         else if(p.val==q.val){
             return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         }
-        else{
+        else
             return false;
-        }
-
     }
 }
