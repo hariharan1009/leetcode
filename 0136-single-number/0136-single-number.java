d54b498bb07218604b4e137807f47ca13e9a4edd
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-07-02 18:16:14 +0530

    Time: 1 ms (99.91%), Space: 46.8 MB (5.52%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int c=0;
         for(int i=0;i<nums.length;i++){
-            c=c^nums[i];
+            c=c^nums[i];  //In bit same number will disappear and differ number will be store;
         }
         return c;
 
