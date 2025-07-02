f56d996ee0960016bc0cc2fdcfa73da15ed062ca
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-07-02 18:31:14 +0530

    Time: 12 ms (21.99%), Space: 45.7 MB (82.2%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            c=c^nums[i];  //In bit same number will disappear and differ number will be store;
-        }
-        return c;
+        //using bit manipulations 
+        // int c=0;
+        // for(int i=0;i<nums.length;i++){
+        //     c=c^nums[i];  //In bit same number will disappear and differ number will be store;
+        // }
+        // return c;
 
-
+         //using array
         // Arrays.sort(nums);
         // for(int i=0;i<nums.length-1;i+=2){
         //     if(nums[i]!=nums[i+1]){
@@ -14,5 +15,17 @@ class Solution {
         //     }
         // }
         // return nums[nums.length-1];
+
+        //using hashmap
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])==1){
+                return nums[i];
+            }
+        }
+        return -1;
     }
 }
