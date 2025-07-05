6be7055b5116cd001609ca05a371ffb3b893dd8e
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-07-05 12:40:37 +0530

    Time: 3 ms (72.75%), Space: 43.6 MB (55.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,39 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder n = new StringBuilder();
-        
-        for (char a : s.toCharArray()) {
-            if (Character.isLetterOrDigit(a)) {
-                n.append(Character.toLowerCase(a));
+        StringBuilder sb = new StringBuilder();
+        for(char i:s.toCharArray()){
+            if(Character.isLetterOrDigit(i)){
+                sb.append(Character.toLowerCase(i));
             }
         }
-       String a=n.toString();
-       int i=0,j=n.length()-1;
-       while(i<j){
-        if(a.charAt(i)!=a.charAt(j)){
-            return false;
+        String a=sb.toString();
+        int st=0,e=a.length()-1;
+        while(st<e){
+            if(a.charAt(st)!=a.charAt(e)){
+                return false;
+            }
+            st++;
+            e--;
         }
-        i++;
-        j--;
-       }
-       return true;
+        return true;
+
+        //Without Using StrinngBuilder
+    //      String a="";
+    //     for(char c:s.toCharArray()){
+    //         if(Character.isDigit(c) || Character.isLetter(c)){
+    //             a+=c;
+    //         }
+    //     }
+    //     a=a.toUpperCase();
+    //     int x=0;
+    //     int y=a.length()-1;
+    //     while(x<=y){
+    //         if(a.charAt(x)!=a.charAt(y)){
+    //             return false;
+    //         }
+    //         x+=1;
+    //         y-=1;
+    //     }
+    // return true;
     }
 }
