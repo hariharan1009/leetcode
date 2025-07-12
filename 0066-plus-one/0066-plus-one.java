bbb8b5717c8fee9c67ad3eb88cb371a947160cb3
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-07-12 21:36:13 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] plusOne(int[] a) {
-        int n=a.length;
+       int n=a.length;
        for(int i=n-1;i>=0;i--){
         if(a[i]<9){
             a[i]++;
@@ -8,7 +8,7 @@ class Solution {
         }
         a[i]=0;
        }
-       a = new int[n +1];
+       a = new int[n+1];
        a[0] = 1;
        return a;
     }
