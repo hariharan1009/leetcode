8fed8ed341fbc1a1e88db5e14de7bb84b1cd2852
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-08-15 20:24:47 +0530

    Time: 2 ms (79.84%), Space: 46.6 MB (13.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -8,18 +8,6 @@ class Solution {
             nums[j]=temp;
             j++;
         }
-    }
-
-
-        // int k=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]!=0){
-        //         nums[k++]=nums[i];
-        //     }
-        // }
-        // for(int i=k;i<nums.length;i++){
-        //     nums[i]=0;
-        // }
-        
-    }
+    }  
+   }
 }
