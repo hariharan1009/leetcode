77b93faf79cdadf9804b3b8d0f79984b8fa5eca5
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-08-28 07:38:59 +0530

    Time: 1 ms (99.73%), Space: 45.1 MB (78.83%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,20 +1,12 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         ArrayList<String> a=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if((i+1)%3==0 && (i+1)%5==0){
-                a.add("FizzBuzz");
-            }
-            else if((i+1)%3==0 ){
-                a.add("Fizz");
-            }
-            else if((i+1)%5==0){
-                a.add("Buzz");
-            }
-            else{
-                a.add(Integer.toString(i+1));
-            }
-        }
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0) a.add("FizzBuzz");
+            else if(i%3==0) a.add("Fizz");
+            else if(i%5==0) a.add("Buzz");
+            else a.add(Integer.toString(i));
+        } 
         return a;
     }
 }
