6b6cb0fc1a6957443018919e645de9c34e1e097d
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-09-20 23:44:23 +0530

    Time: 2 ms (98.05%), Space: 41.7 MB (84.05%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
@@ -2,11 +2,8 @@ class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
         for(int i=1;i<arr.length-1;i++){
-            int diff=Math.abs(arr[i]-arr[i-1]);
-            int diff2=Math.abs(arr[i]-arr[i+1]);
-            if(diff2!=diff){
+            if(Math.abs(arr[i]-arr[i-1])!=Math.abs(arr[i]-arr[i+1]))
                 return false;
-            }
         }
         return true;
     }
