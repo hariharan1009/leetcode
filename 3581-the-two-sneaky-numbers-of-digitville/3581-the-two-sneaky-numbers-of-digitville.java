b1ae9f753b6c1b249c81a2be48d12b746d2a588d
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-10-31 22:03:55 +0530

    Time: 5 ms (4.64%), Space: 45.9 MB (7.71%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,20 +1,5 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        // Arrays.sort(nums);
-        // int[] a=new int[2];
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i-1]==nums[i]){
-        //         a[0]=nums[i];
-        //         break;
-        //     }
-        // }
-        //  for(int i=nums.length-1;i>=0;i--){
-        //     if(nums[i-1]==nums[i]){
-        //         a[1]=nums[i];
-        //         break;
-        //     }
-        // }
-        // return a;
         Arrays.sort(nums);
         int[] a=new int[2];
         int j=0;
@@ -25,5 +10,21 @@ class Solution {
             }
         }
         return a;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i:nums){
+        //     map.put(i,map.getOrDefault(i,0)+1);
+        // }
+        // int[] x=new int[2];
+        // int fl=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.get(nums[i])==2 && fl==0){
+        //         x[0]=nums[i];
+        //         fl=1;
+        //     }
+        //     else if(map.get(nums[i])==2 && fl==1){
+        //         x[1]=nums[i];
+        //     }
+        // }
+        // return x;
     }
 }
