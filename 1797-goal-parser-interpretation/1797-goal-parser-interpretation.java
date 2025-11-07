cb1f9f860295cf19acd9305756c3c28cc960ce8b
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-11-07 19:51:47 +0530

    Time: 1 ms (63.33%), Space: 43 MB (5.14%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String interpret(String command) {
-        return command.replace("()","o").replace("(al)","al"); 
+    public String interpret(String s) {
+        return s.replace("()","o").replace("(al)","al");
     }
 }
