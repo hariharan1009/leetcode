4ee7877ef5600a9067501592ae8df80397345b45
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2025-11-07 12:18:34 +0530

    Time: 6 ms (3.99%), Space: 47.1 MB (5.17%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int[] b=nums.clone();
-        for(int i=0;i<k;i++){
-            Arrays.sort(b);
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==b[0]){
-                    nums[j]*=multiplier;
-                    b[0]*=multiplier;
+        int[] a=nums.clone();
+        while(k-->0){
+            Arrays.sort(a);
+            for(int i=0;i<nums.length;i++){
+                if(a[0]==nums[i]){
+                    nums[i]*=multiplier;
+                    a[0]*=multiplier;
                     break;
                 }
             }
+
         }
         return nums;
     }
