23b0442d097dda2062df8ea9b0d34455effd7b2a
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2026-02-11 07:50:31 +0530

    Time: 0 ms (100%), Space: 48.2 MB (46.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
-                return Math.pow(x,n);
-
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        double p=1;
+        while(n!=0){
+            if((n&1)!=0){
+                p*=x;
+            }
+            x*=x;
+            n>>>=1;
+        }
+        return p;
     }
 }
