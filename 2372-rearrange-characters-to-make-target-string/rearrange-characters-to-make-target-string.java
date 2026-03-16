3a59cac936da9c6892fe4f07ea4b01351cc49f74
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2026-03-16 22:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (89.68%) - LeetSync

---
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rearrangeCharacters(String s, String t) {
+        int[] a=new int[26];
+        int[] b=new int[26];
+        for(int i=0;i<s.length();i++){
+            a[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            b[t.charAt(i)-'a']++;
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+            if(b[i]>0){
+                min=Math.min(a[i]/b[i],min);
+            }
+        }
+        return min;
+    }
+}
