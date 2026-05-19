89dda76e8996d8907c6728e58d59fa297ae47884
HARIHARAN V M <161021578+hariharan1009@users.noreply.github.com>
2026-05-19 21:08:07 +0530

    Time: 16 ms (12.85%) | Memory: 88.7 MB (16.81%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums1){
+            set.add(i);
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(set.contains(nums2[i])){
+                return nums2[i];
+            }
+        }
+        return -1;
+    }
+}
